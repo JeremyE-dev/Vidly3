5920065d052adedc3c6a0c3495a1a700b594a75a
Jeremy Eudaly <jeremy.eudaly@gmail.com>
2022-12-05 20:14:56 -0600

    set up project so it has Identity Config File

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace Vidly3
                       "~/Scripts/popper.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap-lumen.css",
+                      "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly3.Models;
+using Vidly3.ViewModels;
 
 namespace Vidly3.Controllers
 {
@@ -12,9 +13,22 @@ namespace Vidly3.Controllers
         // GET: Movies/Random
         public ActionResult Random()
         {
-            var movie = new Movie() {Name = "Shrek!" };
             
-            return View(movie);
+            var movie = new Movie() {Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer1" },
+                new Customer {Name = "Customer2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            
+            //need to change the model in teh view to accept this viewmodel
+            return View(viewModel);
         }
     }
 }
